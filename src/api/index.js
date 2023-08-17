959941c1387719153facac0ed8744d5d01fc0f8c
arvind1257 <97084780+arvind1257@users.noreply.github.com>
2023-08-17 13:04:33 +0530

    done

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({baseURL:'https://expensive-undershirt-bee.cyclic.app'})
+const API = axios.create({baseURL:'https://yellow-coati-hose.cyclic.cloud'})
 //const API = axios.create({baseURL:'http://localhost:5000'})
 export const logIn = (authData) => API.post('/user/login',authData)
 export const signUp = (authData) => API.post('/user/signup',authData)
